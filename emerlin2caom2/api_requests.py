0357760b9012e6bfde82674a31ed2002aae66c37
Erin <erin.brassfieldbourke@manchester.ac.uk>
2025-04-08 10:49:20 +0100

    Abstract api calls to api_requests.py
    Organise code, move all the http calls to a separate file, api_requests.py.  Left ingest_manager function inside of main_app.py for now.

---
+++ emerlin2caom2/api_requests.py
@@ -0,0 +1,101 @@
+import requests
+import pyvo as vo
+
+
+def request_post(self, xml_output_name):
+    """
+    Post (new) target XML data onto the database.
+    Note this is flipped and was formerly 'put' in torkeep.
+    In recommended curl command, --data item has an @ before it. --data "@TS8004_C_001_20190801_De.xml"
+    :param xml_output_name: ObservationID of xml file to post
+    """
+    post_file = xml_output_name
+    xml_output_name = "@" + xml_output_name
+    url_post = self.base_url
+    headers_post = {'Content-type': 'application/xml', 'accept': 'application/xml'}
+    res = requests.post(url_post, data=open(post_file, 'rb'), verify=self.rootca, headers=headers_post)
+    # print(res.status_code) # can remove once code no longer needs debugging
+    return res.status_code
+
+def request_delete(self, to_del):
+    """
+    Deletes target XML data on the database.
+    :param to_del: ObservationID of target data to delete
+    """
+    url_del = self.base_url + '/' + to_del
+    # print(url_del) # can remove once code no longer needs debugging
+    res = requests.delete(url_del, verify=self.rootca)
+    if res.status_code == 204:
+        print(to_del + " has been deleted.")
+    else:
+        print(res.status_code + ": Delete may have failed for " + to_del) # can remove once code no longer needs debugging
+    return res.status_code
+
+def request_delete(self, to_del):
+    """
+    Deletes target XML data on the database.
+    :param to_del: ObservationID of target data to delete
+    """
+    url_del = self.base_url + '/' + to_del
+    # print(url_del) # can remove once code no longer needs debugging
+    res = requests.delete(url_del, verify=self.rootca)
+    if res.status_code == 204:
+        print(to_del + " has been deleted.")
+    else:
+        print(res.status_code + ": Delete may have failed for " + to_del) # can remove once code no longer needs debugging
+    return res.status_code
+
+def request_get(self, file_to_get=''):
+    """
+    Get target data from database based on observations/uri. NOT YET FUNCTIONAL WITH API.
+    :param file_to_get: ObservationID of file to get
+    """
+    payload = {'uri': file_to_get}
+    #url_get = self.base_url + '/' + file_to_get
+    url_get = self.base_url
+    print(url_get) # can remove once code no longer needs debugging
+    res = requests.get(url_get, params=payload, verify=self.rootca)
+    print(res) # can remove once code no longer needs debugging
+
+
+def find_existing(self, obs_id):
+    """
+    Use pyvo TAP service to query for existing record before
+    new record is inserted, or before delete/update.
+    :param obs_id: observation uri or unique identifier.
+    :returns: if exists, the uuid aka primary key for db record(s) for this uri.
+    """
+    url_tap = self.base_url.split('/observations')[0] + '/tap'
+    service = vo.dal.TAPService(url_tap)
+    uuid_query = "SELECT id FROM Observation WHERE uri="+"'"+obs_id+"'"
+    resultset = service.search(uuid_query)
+    if len(resultset) > 1:
+        print("Duplicate Records found for: " + obs_id)
+        for row in resultset:
+            print(row['id'])
+        return resultset
+        # Add Error logging here.
+    elif len(resultset) == 1:
+        # print(resultset[0]['id'])
+        return resultset[0]['id']
+    else:
+        print("No existing record found for " + obs_id + ". Ok to ingest.")
+        # Add Error logging here.
+
+def request_tap(self, obs_id):
+    """
+    Use tap service to query for existence of observation and return uuid
+    :param obs_id: observation id or meaningful uri of the observation record.
+    :returns machine_id: The record id used in the database as primary key.
+    """
+    url_tap = self.base_url.split('/observations')[0] + '/tap/sync?REQUEST=doQuery&LANG=ADQL&FORMAT=json&QUERY=SELECT+id+FROM+Observation+WHERE+uri=%27' + obs_id + '%27'
+
+    if url_tap:
+        print('location: ' + url_tap)
+        res = requests.get(url_tap, verify=self.rootca)
+        print(res)
+        print(res.text)
+        print(res.text[1][0])
+    else:
+        print("tap didn't work.")
+
--- emerlin2caom2/main_app.py
+++ emerlin2caom2/main_app.py
@@ -16,6 +16,7 @@ import casa_reader as casa
 import file_metadata as msmd
 import fits_reader as fr
 import settings_file as set_f
+import api_requests as api
 
 __all__ = [
     'EmerlinMetadata',
@@ -457,15 +458,15 @@ class EmerlinMetadata:
         :param xml_output_name: xml file containing metadata to ingest.  
         """ 
         if set_f.upload:
-            machine_id = self.find_existing(obs_uri)
+            machine_id = api.find_existing(self, obs_uri)
             if machine_id:
                 if (set_f.replace_old_data) and isinstance(machine_id, str):
-                    del_stat = self.request_delete(machine_id)
+                    del_stat = api.request_delete(self, machine_id)
                     if del_stat == 204:
                         print(obs_uri + " deleted.")
                     else:
                         print(obs_uri + " attempted delete with status code " + del_stat)
-                    create_stat = self.request_post(xml_output_name)
+                    create_stat = api.request_post(self, xml_output_name)
                     if create_stat == 201:
                         print(obs_uri + " ingested.")
                     else:
@@ -473,123 +474,10 @@ class EmerlinMetadata:
                 else:
                     print("Multiple records found; no action taken.")
             else:
-                create_stat = self.request_post(xml_output_name)
+                create_stat = api.request_post(self, xml_output_name)
                 if create_stat == 201:
                     print(obs_uri + " ingested.")
                 else:
                     print(obs_uri + "attempted update with status code: " + create_stat)
 
 
-    def url_maker(self, xml_output_name):
-        """
-        Construction of a URL to be used within a request to the archive service.
-        :param xml_output_name: ObservationID of object to be targeted by URL
-        :returns: URL of the target object and target torkeep collection
-        """
-        # Removing the url ending based on obsid as current archive-service uses DB-generated uuid for 
-        # url ending instead. Considering update to java api to include method for obs_id.
-        #
-        # made_url = self.base_url + '/' + '.'.join(xml_output_name.split('/')[-1].split('.')[:-1]).rstrip()
-        made_url = self.base_url   # Remove/fix once decision taken on how to name url-ending.
-        return made_url
-
-    def request_post(self, xml_output_name):
-        """
-        Post (new) target XML data onto the database. 
-        Note this is flipped and was formerly 'put' in torkeep.
-        In recommended curl command, --data item has an @ before it. --data "@TS8004_C_001_20190801_De.xml"
-        :param xml_output_name: ObservationID of xml file to post
-        """
-        post_file = xml_output_name
-        xml_output_name = "@" + xml_output_name
-        url_post = self.url_maker(xml_output_name)
-        headers_post = {'Content-type': 'application/xml', 'accept': 'application/xml'}
-        res = requests.post(url_post, data=open(post_file, 'rb'), verify=self.rootca, headers=headers_post)
-        # print(res.status_code) # can remove once code no longer needs debugging
-        return res.status_code
-
-
-    def request_put(self, xml_output_name):
-        """
-        Put (update) target XML data to the database. NEEDS UPDATING; currently unused.
-        Note this is flipped and was formerly 'post' in torkeep.
-        :param xml_output_name: ObservationID of target xml data
-        """
-        xml_output_name = xml_output_name.rstrip()
-        url_put = self.url_maker(xml_output_name)
-        print('put: '+repr(url_put)) # can remove once code no longer needs debugging
-        put_file = xml_output_name
-        print(put_file) # can remove once code no longer needs debugging
-        headers_put = {'Content-type': 'application/xml', 'accept': 'application/xml'}
-        res = requests.put(url_put, data=open(put_file, 'rb'), verify=self.rootca, headers=headers_put)
-        print(res, res.content) # can remove once code no longer needs debugging
-        print("URL:", url_put) # can remove once code no longer needs debugging
-        print("Response Code:", res.status_code) # can remove once code no longer needs debugging
-        print("Response Text:", res.text) # can remove once code no longer needs debugging
-
-    def request_delete(self, to_del):
-        """
-        Deletes target XML data on the database.
-        :param to_del: ObservationID of target data to delete
-        """
-        url_del = self.base_url + '/' + to_del
-        # print(url_del) # can remove once code no longer needs debugging
-        res = requests.delete(url_del, verify=self.rootca)
-        if res.status_code == 204:
-            print(to_del + " has been deleted.") 
-        else:
-            print(res.status_code + ": Delete may have failed for " + to_del) # can remove once code no longer needs debugging
-        return res.status_code
-
-    def request_get(self, file_to_get=''):
-        """
-        Get target data from database based on observations/uri. NOT YET FUNCTIONAL WITH API.
-        :param file_to_get: ObservationID of file to get
-        """
-        payload = {'uri': file_to_get}
-        #url_get = self.base_url + '/' + file_to_get
-        url_get = self.base_url
-        print(url_get) # can remove once code no longer needs debugging
-        res = requests.get(url_get, params=payload, verify=self.rootca)
-        print(res) # can remove once code no longer needs debugging
-
-    def find_existing(self, obs_id):
-        """
-        Use pyvo TAP service to query for existing record before 
-        new record is inserted, or before delete/update.
-        :param obs_id: observation uri or unique identifier.
-        :returns: if exists, the uuid aka primary key for db record(s) for this uri.
-        """
-        url_tap = self.base_url.split('/observations')[0] + '/tap'
-        service = vo.dal.TAPService(url_tap)
-        uuid_query = "SELECT id FROM Observation WHERE uri="+"'"+obs_id+"'"
-        resultset = service.search(uuid_query)
-        if len(resultset) > 1:
-            print("Duplicate Records found for: " + obs_id)
-            for row in resultset:
-                print(row['id'])
-            return resultset
-            # Add Error logging here.
-        elif len(resultset) == 1:
-            # print(resultset[0]['id'])
-            return resultset[0]['id']
-        else:
-            print("No existing record found for " + obs_id + ". Ok to ingest.")
-            # Add Error logging here.
-
-    def request_tap(self, obs_id):
-        """
-        Use tap service to query for existence of observation and return uuid 
-        :param obs_id: observation id or meaningful uri of the observation record.
-        :returns machine_id: The record id used in the database as primary key.
-        """
-        url_tap = self.base_url.split('/observations')[0] + '/tap/sync?REQUEST=doQuery&LANG=ADQL&FORMAT=json&QUERY=SELECT+id+FROM+Observation+WHERE+uri=%27' + obs_id + '%27'
-
-        if url_tap:
-            print('location: ' + url_tap)
-            res = requests.get(url_tap, verify=self.rootca)
-            print(res)
-            print(res.text) 
-            print(res.text[1][0])
-        else:
-            print("tap didn't work.")
