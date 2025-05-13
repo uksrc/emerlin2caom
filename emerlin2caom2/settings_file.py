b34d9516b7453822d6af082392f41e55c65e324e
mj1e16lsst <michael.johnson0100@gmail.com>
2025-05-13 15:19:03 +0100

    Update settings_file.py
    removing rootca from settings file

--- emerlin2caom2/settings_file.py
+++ emerlin2caom2/settings_file.py
@@ -4,4 +4,3 @@ xmldir = '' # directory where xml file will be generated, e.g. './data/'
 upload = True
 replace_old_data = True
 base_url = '' # location of database e.g. 'https://src-data-repo.co.uk/torkeep/observations/EMERLIN'
-rootca = '' # location of rootCA.pem file
